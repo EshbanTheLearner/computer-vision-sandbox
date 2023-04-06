9e0faa23b6681ca165d28b5b887bd5435740e2f2
Eshban Suleman <eshban9492@gmail.com>
2023-04-06 12:46:16 +0500

    download data code

--- object-detection/single-object-detection.py
+++ object-detection/single-object-detection.py
@@ -20,3 +20,19 @@ import matplotlib.pyplot as plt
 import os
 import collections
 import copy
+
+path2data = "./data"
+
+if not os.path.exists(path2data):
+    os.mkdir(path2data)
+
+data_transformer = transforms.Compose([
+    transforms.ToTensor()
+])
+
+train_ds = datasets.STL10(
+    path2data, split="train", 
+    download=True, transform=data_transformer
+)
+
+print(train_ds.data.shape)
