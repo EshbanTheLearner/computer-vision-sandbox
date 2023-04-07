fb4a725b3287fae5e8067e12931eb122d9eeca74
EshbanTheLearner <eshban9492@gmail.com>
2023-04-07 12:05:21 +0500

    Defined calc_gt_offsets function

--- object-detection/utils.py
+++ object-detection/utils.py
@@ -37,8 +37,16 @@ def parse_annotation(annotation_path, image_dir, img_size):
         gt_classes_all.append(groundtruth_classes)
     return gt_boxes_all, gt_classes_all, img_paths
 
-def calc_gt_offsets(pos_anc_coords, ggt_bbox_mapping):
-    pass
+def calc_gt_offsets(pos_anc_coords, gt_bbox_mapping):
+    pos_anc_coords = ops.box_convert(pos_anc_coords, in_fmt="xyxy", out_fmt="cxcywh")
+    gt_bbox_mapping = ops.box_convert(gt_bbox_mapping, in_fmt="xyxy", out_fmt="cxcywh")
+    gt_cx, gt_cy, gt_w, gt_h = gt_bbox_mapping[:, 0], gt_bbox_mapping[:, 1], gt_bbox_mapping[: ,2], gt_bbox_mapping[:, 3]
+    anc_cx, anc_cy, anc_w, anc_h = pos_anc_coords[:, 0], pos_anc_coords[:, 1], pos_anc_coords[:, 2], pos_anc_coords[:, 3]
+    tx_ = (gt_cx - anc_cx)/anc_w
+    ty_ = (gt_cy - anc_cy)/anc_h
+    tw_ = torch.log(gt_w/anc_w)
+    th_ = torch.log(gt_h/anc_h)
+    return torch.stack([tx_, ty_, tw_, th_], dim=-1)
 
 def gen_anc_centers(out_size):
     pass
