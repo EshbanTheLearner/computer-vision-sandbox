d9c307f39f87ab3fbd15a5f3d0d621f1d0e10fab
Eshban Suleman <eshban9492@gmail.com>
2023-04-09 00:08:28 +0500

    Added output_boxes function

--- object-detection/YOLOv3/utils.py
+++ object-detection/YOLOv3/utils.py
@@ -8,7 +8,35 @@ def resize_image(inputs, model_size):
     return inputs
 
 def output_boxes(inputs, model_size, max_output_size, max_output_size_per_class, iou_threshold, confidence_threshold):
-    pass
+    center_x, center_y, width, height, confidence, classes = tf.split(
+        inputs, 
+        [1, 1, 1, 1, 1, -1],
+        axis=-1
+    )
+    top_left_x = center_x - width / 2.0
+    top_left_y = center_y - height / 2.0
+    bottom_right_x = center_x + width / 2.0
+    bottom_right_y = center_y + height / 2.0
+
+    inputs = tf.concat([
+        top_left_x,
+        top_left_y,
+        bottom_right_x,
+        bottom_right_y,
+        confidence,
+        classes
+    ], axis=-1)
+
+    boxes_dicts = non_max_suppression(
+        inputs,
+        model_size,
+        max_output_size,
+        max_output_size_per_class,
+        iou_threshold,
+        confidence_threshold
+    )
+
+    return boxes_dicts
 
 def draw_outputs(img, boxes, objectness, classes, nums, class_names):
     pass
